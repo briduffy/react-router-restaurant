d55540f770e7c1f64d862629ebcfa93ee0570121
Brianne Duffy <duffymbrianne@gmail.com>
2018-09-17 17:04:06 -0600

    main page, details page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Route } from 'react-router-dom'
 import Main from './components/Main'
+import Details from './components/Details'
 
 const App = () => (
+<Fragment>
   <Route exact path="/" component={Main} />
+  <Route exact path="/details" component={Details} />
+</Fragment>
 )
 
 export default App
