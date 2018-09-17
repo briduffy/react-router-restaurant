d2fc451600250dcba74a88ca29ceec768fa2e578
Brianne Duffy <duffymbrianne@gmail.com>
2018-09-17 17:26:59 -0600

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from 'react'
-import { Route } from 'react-router-dom'
+import React, { Fragment} from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Main from './components/Main'
 import Details from './components/Details'
+import NoMatch from './components/NoMatch'
+import NavBar from './components/NavBar'
 
 const App = () => (
 <Fragment>
-  <Route exact path="/" component={Main} />
-  <Route exact path="/details" component={Details} />
+  <NavBar />
+  <Switch>
+    <Route exact path="/" component={Main} />
+    <Route exact path="/details" component={Details} />
+    <Route component={NoMatch} />
+  </Switch>
 </Fragment>
 )
 
--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -2,24 +2,18 @@ import React from 'react'
 
 const Details = () => (
   <div className="container">
-    <div className="Header">
-    <header>
-      <h1>Location</h1>
-    </header>
-      <p>370 300 E, Salt Lake City, UT 84111</p>
+    <h1>Location</h1>
+    <p>370 300 E, Salt Lake City, UT 84111</p>
     <br />
     <h1>Hours</h1>
     <h3>Monday-Friday</h3>
     <p>1:00pm - 11:00pm</p>
     <h3>Saturday</h3>
     <p>9:00am - 12:00am</p>
-    <br />
     <h3>Closed on Sunday</h3>
     <br />
     <h1>Call Us</h1>
     <p>(801) 448-7240</p>
-    </div>
-
   </div>
 )
 
